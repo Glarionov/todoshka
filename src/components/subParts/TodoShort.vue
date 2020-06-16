eb6ab3bf2ba851b7b2d2b3029e3e996dae160531
Glarionov <artevy@mail.ru>
2020-06-16 23:07:16 +0300

    basic parts

---
+++ src/components/subParts/TodoShort.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "TodoShort"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/subParts/TodoShortPart.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "TodoShortPart"
+    }
+</script>
+
+<style scoped>
+
+</style>
